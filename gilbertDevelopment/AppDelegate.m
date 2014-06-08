0b16ba2ea954a13e08e4206624caa5a16eaf9751
Hugh Rawlinson <hugh@codeoclock.net>
2014-06-08 17:11:05 +0100

    not sure

--- gilbertDevelopment/AppDelegate.m
+++ gilbertDevelopment/AppDelegate.m
@@ -7,15 +7,40 @@
 //
 
 #import "AppDelegate.h"
-#import "gilbertcore/gilbert.h"
+#import "RtAudio.h"
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
+int record( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames, double streamTime, RtAudioStreamStatus status, void *userData ){
+    if(status){
+        std::cout << "Stream overflow detected!" << std::endl;
+    }
+    // Do something with the data in the "inputBuffer" buffer.
+    return 0;
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
-    gilbert * g = new gilbert();
-    std::cout<<g->test()<<std::endl;
+    RtAudio adc;
+    if ( adc.getDeviceCount() < 1 ) {
+        std::cout << "\nNo audio devices found!\n";
+        exit( 0 );
+    }
+    RtAudio::StreamParameters parameters;
+    parameters.deviceId = adc.getDefaultInputDevice();
+    parameters.nChannels = 2;
+    parameters.firstChannel = 0;
+    unsigned int sampleRate = 44100;
+    unsigned int bufferFrames = 256; // 256 sample frames
+    try {
+        adc.openStream( NULL, &parameters, RTAUDIO_SINT16,
+                       sampleRate, &bufferFrames, &record );
+        adc.startStream();
+    }
+    catch ( RtAudioError& e ) {
+        e.printMessage();
+        exit( 0 );
+    }
 }
 
 @end
